270e2c9f025196bb03b6618c27522123f991fa58
Julian <birdman.coding.1991@gmail.com>
2024-07-01 10:09:36 +0200

    update tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          DEFAULT: "#1C1C27",
+          light: "#363740",
+        },
+        white: {
+          DEFAULT: "#FFFFFF",
+          dimmed: "rgba(255,255,255,0.4)",
+          "dimmed-heavy": "rgba(255,255,255,0.2)",
+        },
+        yellow: "#FFB43A",
+        red: "#EF4444",
+        green: "#22C55E",
+      },
+    },
   },
   plugins: [],
 };
