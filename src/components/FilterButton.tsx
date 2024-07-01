ac555248b47251c50f7012efa5f0fb6e69910fcf
Nikita Nakropin <nikita@nakropin.com>
2024-07-01 14:41:28 +0200

    added functionality and tweaked style

--- src/components/FilterButton.tsx
+++ src/components/FilterButton.tsx
@@ -1,12 +1,29 @@
-import { genre } from "../utils/types";
+type Props = {
+  name: string;
+  icon: string;
+  isSelected: boolean;
+  onClick: () => void;
+};
 
-export default function FilterButton({ name, icon }: genre) {
+export default function FilterButton({
+  name,
+  icon,
+  isSelected,
+  onClick,
+}: Props) {
   return (
-    <div className="flex flex-col gap-2 items-center justify-center text-center bg-dark m-4">
-      <div className="bg-white-dimmed-heavy h-16 w-16 flex items-center justify-center rounded-xl text-3xl">
+    <div
+      className={`flex flex-col items-center justify-center text-center bg-dark
+      `}
+      onClick={onClick}
+    >
+      <div
+        className={`h-16 w-16 flex items-center justify-center text-3xl my-2 rounded-xl
+        ${isSelected ? "bg-white-dimmed" : "bg-white-dimmed-heavy"}`}
+      >
         {icon}
       </div>
-      <div className="text-white-dimmed">{name}</div>
+      <div className="text-white-dimmed text-xs">{name}</div>
     </div>
   );
 }
--- src/layouts/GenreOverview.tsx
+++ src/layouts/GenreOverview.tsx
@@ -1,12 +1,41 @@
+import { useState } from "react";
 import { genres } from "../utils/data";
 import FilterButton from "../components/FilterButton";
 
 export default function GenreOverview() {
+  const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
+
+  const toggleGenre = (name: string) => {
+    setSelectedGenres((prevSelectedGenres) =>
+      prevSelectedGenres.includes(name)
+        ? prevSelectedGenres.filter((genre) => genre !== name)
+        : [...prevSelectedGenres, name]
+    );
+  };
+
   return (
-    <div className="grid grid-flow-row grid-cols-4 text-center bg-dark">
-      {genres.map((genre) => (
-        <FilterButton name={genre.name} icon={genre.icon} />
-      ))}
-    </div>
+    <section className="flex flex-col bg-dark px-8">
+      <div className="flex flex-row justify-between">
+        <span className="text-white">{"<"}</span>
+        <h1 className="text-white">Genres</h1>
+        <span></span>
+      </div>
+      <div className="grid grid-flow-row grid-cols-4 text-center gap-4">
+        {genres.map((genre) => (
+          <FilterButton
+            key={genre.name}
+            name={genre.name}
+            icon={genre.icon}
+            isSelected={selectedGenres.includes(genre.name)}
+            onClick={() => toggleGenre(genre.name)}
+          />
+        ))}
+      </div>
+      <div className="flex flex-row">
+        <span className="text-white">{selectedGenres.length}&nbsp;</span>
+        <span className="text-white-dimmed">genres selected</span>
+      </div>
+      <button className="bg-yellow text-white">Confirm selected Genres</button>
+    </section>
   );
 }
