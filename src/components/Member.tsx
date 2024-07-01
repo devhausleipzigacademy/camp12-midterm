4484f614cf46e39de45d3a384093124e1e8a90c4
Malibu <joellement@gmail.com>
2024-07-01 15:27:46 +0200

    adjusted styling as requested

--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -7,14 +7,10 @@ type Props = {
 const Member = ({ image, name, role }: Props) => {
   return (
     <>
-      <div className="flex flex-row h-16 gap-6 justify-center items-center">
-        <img
-          src={image}
-          alt={name}
-          className="h-16 aspect-square object-cover"
-        />
+      <div className="flex gap-5 justify-center items-center">
+        <img src={image} alt={name} className="size-16 object-cover" />
         <div>
-          <h2 className="text-white font-bold text-sm">{name}</h2>
+          <h2 className="text-white font-bold text-sm mb-1">{name}</h2>
           <p className="text-white-dimmed text-xs">{role}</p>
         </div>
       </div>
