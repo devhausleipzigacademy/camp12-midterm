c558898b0b6f0425d7822a0f3feb7df95155ba25
Fred <frederik.koenig@posteo.de>
2024-07-01 14:30:32 +0200

    created seats-component

---
+++ src/components/seat.tsx
@@ -0,0 +1,20 @@
+import { Checkbox } from "@headlessui/react";
+
+type Props = {
+  disabled: boolean;
+  selected: boolean;
+  onChange: (value: boolean) => void;
+};
+
+export function Seat({ disabled, selected, onChange }: Props) {
+  return (
+    <>
+      <Checkbox
+        checked={selected}
+        onChange={onChange}
+        disabled={disabled}
+        className="group block size-7 rounded border bg-dark-light data-[checked]:bg-yellow data-[disabled]:cursor-not-allowed data-[disabled]:bg-white"
+      ></Checkbox>
+    </>
+  );
+}
