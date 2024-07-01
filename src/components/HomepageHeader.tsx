10f85b05fc9feee6e7c8e31415a797045c523378
multimediaschneider <schneider-sven@posteo.de>
2024-07-01 16:51:51 +0200

    create-homepage-header

--- src/components/HomepageHeader.tsx
+++ src/components/HomepageHeader.tsx
@@ -1,8 +1,29 @@
-export function HomepageHeader() {
+import React from "react";
+
+interface HomepageHeaderProps {
+  userName: string;
+  userImage: string;
+}
+
+export const HomepageHeader: React.FC<HomepageHeaderProps> = ({
+  userName,
+  userImage,
+}) => {
   return (
-    <div className="flex w-screen bg-dark pl-5 pr-5 pt-8 text-white">
-      <div className="flex-col"></div>
-      <div></div>
+    <div className="flex w-screen h-10 bg-dark pl-5 pr-5 pt-8 text-white justify-between items-center">
+      <div className="flex-col">
+        <div className="pb-3 text-white-dimmed text-xs">
+          Welcome {userName} <span className="text-white">👋</span>
+        </div>
+        <div className="text-sm">Let's relax and watch a movie!</div>
+      </div>
+      <div className="bg-white w-10 h-10 rounded-full">
+        <img
+          src={userImage}
+          alt={`${userName}'s avatar`}
+          className="w-full h-full rounded-full"
+        />
+      </div>
     </div>
   );
-}
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,79 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import "./index.css";
-import { LoginPage } from "./routes/LoginPage";
-
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/genres",
-    element: <p>Genre Page</p>,
-  },
-  {
-    path: "/",
-    element: (
-      <div>
-        <p>Navbar</p>
-        <Outlet />
-      </div>
-    ),
-    children: [
-      {
-        index: true,
-        element: <p>Home Page</p>,
-      },
-      {
-        path: "movies",
-        element: <p>Movies Page</p>,
-      },
-      {
-        path: "bookmarks",
-        element: <p>Bookmarks Page</p>,
-      },
-      {
-        path: "profile",
-        element: <p>Profile Page</p>,
-      },
-    ],
-  },
-  {
-    path: "/movies/:movieId",
-    element: (
-      <div>
-        <p>Full Page</p>
-        <Outlet />
-      </div>
-    ),
-    children: [
-      {
-        index: true,
-        element: <p>Movie Details Page</p>,
-      },
-      {
-        path: "cast-and-crew",
-        element: <p>Cast and Crew Page</p>,
-      },
-      {
-        path: "select-time",
-        element: <p>Select Time Page</p>,
-      },
-      {
-        path: "select-seats",
-        element: <p>Select Seats Page</p>,
-      },
-      {
-        path: "ticket",
-        element: <p>Ticket Page</p>,
-      },
-    ],
-  },
-]);
+import { HomepageHeader } from "./components/HomepageHeader";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HomepageHeader userName={"Heinz-Werner"} userImage={""} />
+    <div className="bg-dark p-20 h-screen grid grid-cols-4 gap-20">
+      <div className="bg-dark-light"></div>
+      <div className="bg-white"></div>
+      <div className="bg-white-dimmed"></div>
+      <div className="bg-white-dimmed-heavy"></div>
+      <div className="bg-yellow"></div>
+      <div className="bg-red"></div>
+      <div className="bg-green"></div>
+    </div>
   </React.StrictMode>
 );
