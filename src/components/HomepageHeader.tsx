3e08a1843ba1481680f196ba2028bb88b7b463b3
multimediaschneider <schneider-sven@posteo.de>
2024-07-01 14:58:55 +0200

    homepage-header

---
+++ src/components/HomepageHeader.tsx
@@ -0,0 +1,8 @@
+export function HomepageHeader() {
+  return (
+    <div className="flex w-screen bg-dark pl-5 pr-5 pt-8 text-white">
+      <div className="flex-col"></div>
+      <div></div>
+    </div>
+  );
+}
