bd6f9d34f4c47a89ed0cdc5359f82a63a301f3b9
jsf@posteo.de <38862519+jsf030@users.noreply.github.com>
2024-07-01 16:15:46 +0200

    create button component

---
+++ src/components/button.tsx
@@ -0,0 +1,34 @@
+/*
+Create a button that has 2 variants (primary, secondary) and 2 sizes (default, small)
+A button can be also disabled
+The button can take text to display
+*/
+
+import { cn } from "../utils/styling";
+
+type Props = {
+  children: React.ReactNode;
+  variant?: "primary" | "secondary" | "ghost";
+  size?: "default" | "small";
+};
+
+export function Button({
+  children,
+  variant = "primary",
+  size = "default",
+}: Props) {
+  return (
+    <button
+      className={cn(
+        "rounded-lg font-semibold",
+        "hover:shadow-xl",
+        variant === "primary" ? "bg-yellow disabled:bg-yellow/50" : null,
+        variant === "secondary" ? "bg-dark-light" : null,
+        size === "default" ? "py-4 w-full" : null,
+        size === "small" ? "py-2 w-full" : null
+      )}
+    >
+      {children}
+    </button>
+  );
+}
