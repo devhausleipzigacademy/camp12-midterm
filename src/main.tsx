1720a931671985de750a20430a5cc320b00f60cb
Malibu <joellement@gmail.com>
2024-07-02 11:54:19 +0200

    started, but not finished

--- src/main.tsx
+++ src/main.tsx
@@ -1,83 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-import "./index.css";
-import { Homepage } from "./routes/Homepage";
-import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+import CastPage from "./routes/cast-page";
 
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <p>Login Page</p>,
-  },
-  {
-    path: "/genres",
-    element: <p>Genre Page</p>,
-  },
-  {
-    path: "/",
-    element: (
-      <div>
-        <p>Navbar</p>
-        <Outlet />
-      </div>
-    ),
-    children: [
-      {
-        index: true,
-        element: <Homepage />,
-      },
-      {
-        path: "movies",
-        element: <p>Movies Page</p>,
-      },
-      {
-        path: "bookmarks",
-        element: <p>Bookmarks Page</p>,
-      },
-      {
-        path: "profile",
-        element: <p>Profile Page</p>,
-      },
-    ],
-  },
-  {
-    path: "/movies/:movieId",
-    element: (
-      <div>
-        <p>Full Page</p>
-        <Outlet />
-      </div>
-    ),
-    children: [
-      {
-        index: true,
-        element: <p>Movie Details Page</p>,
-      },
-      {
-        path: "cast-and-crew",
-        element: <p>Cast and Crew Page</p>,
-      },
-      {
-        path: "select-time",
-        element: <p>Select Time Page</p>,
-      },
-      {
-        path: "select-seats",
-        element: <p>Select Seats Page</p>,
-      },
-      {
-        path: "ticket",
-        element: <p>Ticket Page</p>,
-      },
-    ],
-  },
-]);
+import "./index.css";
+
+import { NavBar } from "./components/nav-bar";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    {/* <NavBar></NavBar>
+    <NavBar></NavBar>
+    <CastPage
+      name={"Jake Jakeson"}
+      role={"Foo the Fork"}
+      image={"https://i.imgflip.com/qixjk.jpg"}
+    ></CastPage>
     <div className="bg-dark p-20 h-screen grid grid-cols-4 gap-20">
       <Member
         name={"Jake Jakeson"}
