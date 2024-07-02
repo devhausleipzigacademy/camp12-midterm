f7a3a11e48e112462d7b3ec412b5ebc45cac47d7
Dan McAtee <danielmcatee@me.com>
2024-07-02 14:47:39 +0200

    Update Label.tsx

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -9,7 +9,7 @@ export function Label({ selected, disabled, children, onClick }: Props) {
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`font-inter font-medium text-sm/4 text-center rounded disabled:text-white-dimmed-heavy ${
+      className={`font-inter font-medium text-sm/4 text-center rounded disabled:text-white-dimmed-heavy py-2.5 px-4 ${
         selected ? "bg-yellow text-dark-light" : "text-white-dimmed"
       }`}
     >{children}</button>
