cbd2ddcf67d6fae21c12c6816a46c25d9ca8500d
Dan McAtee <danielmcatee@me.com>
2024-07-02 11:21:03 +0200

    put w-full in base styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,9 +1,3 @@
-/*
-Create a button that has 2 variants (primary, secondary) and 2 sizes (default, small)
-A button can be also disabled
-The button can take text to display
-*/
-
 import { cn } from "../utils/styling";
 
 type Props = {
@@ -20,11 +14,11 @@ export function Button({
   return (
     <button
       className={cn(
-        "rounded-lg font-semibold",
+        "rounded-lg font-semibold w-full",
         variant === "primary" ? "bg-yellow disabled:bg-yellow/50" : null,
         variant === "secondary" ? "bg-dark-light" : null,
-        size === "default" ? "py-4 w-[100%]" : null,
-        size === "small" ? "text-xs py-3 w-[100%]" : null
+        size === "default" ? "py-4" : null,
+        size === "small" ? "text-xs py-3" : null
       )}
     >
       {children}
