cebea427c74b4ea0bd16075745c37319abeeae90
Malibu <joellement@gmail.com>
2024-07-02 14:12:07 +0200

    almost done

--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -7,11 +7,14 @@ export type Props = {
 export const Member = ({ image, name, role }: Props) => {
   return (
     <>
-      <div className="flex gap-5 justify-center items-center">
-        <img src={image} alt={name} className="size-16 object-cover" />
-        <div>
-          <h2 className="text-white font-bold text-sm mb-1">{name}</h2>
-          <p className="text-white-dimmed text-xs">{role}</p>
+      <div className="col-span-full row-span-1">
+        {" "}
+        <div className="flex gap-5 items-center">
+          <img src={image} alt={name} className="size-16 object-cover" />
+          <div>
+            <h2 className="text-white font-bold text-sm mb-1">{name}</h2>
+            <p className="text-white-dimmed text-xs">{role}</p>
+          </div>
         </div>
       </div>
     </>
