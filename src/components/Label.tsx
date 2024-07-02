be46d5f2d50431ea426b1f7f46be3d16061afca4
Dan McAtee <danielmcatee@me.com>
2024-07-02 14:44:18 +0200

    Update Label.tsx

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,6 +1,7 @@
 type Props = {
   selected: boolean;
   disabled: boolean;
+  children: React.ReactNode
 };
 export function Label({ selected, disabled }: Props) {
   return (
@@ -9,6 +10,6 @@ export function Label({ selected, disabled }: Props) {
       className={`font-inter font-medium text-sm/4 text-center rounded disabled:text-white-dimmed-heavy ${
         selected ? "bg-yellow text-dark-light" : "text-white-dimmed"
       }`}
-    ></button>
+    >{children}</button>
   );
 }
