ab80ceb36f0d33b05b5b571b4ae6219a872dbc90
Dan McAtee <danielmcatee@me.com>
2024-07-02 11:42:57 +0200

    fix file naming

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Member from "./components/Member";
-import { NavBar } from "./components/NavBar";
+import { NavBar } from "./components/nav-bar";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NavBar></NavBar>
     <div className="bg-dark p-20 h-screen grid grid-cols-4 gap-20">
-      <Member
-        name={"Jake Jakeson"}
-        role={"Foo the Fork"}
-        image={"https://i.imgflip.com/qixjk.jpg"}
-      />
       <div className="bg-dark-light"></div>
       <div className="bg-white"></div>
       <div className="bg-white-dimmed"></div>
