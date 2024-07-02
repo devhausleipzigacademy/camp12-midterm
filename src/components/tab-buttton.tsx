5326e5b11d1ffcadda0f6ad3958c5690d99fd931
Dan McAtee <danielmcatee@me.com>
2024-07-02 11:31:28 +0200

    chage to consistent naming

--- src/components/tab-buttton.tsx
+++ src/components/tab-buttton.tsx
@@ -1,20 +1,20 @@
 type Props = {
-	label: string;
-	isSelected: boolean;
-	clickedOn: () => void;
+  label: string;
+  selected: boolean;
+  onSelect: () => void;
 };
 
-export function TabButton({ label, isSelected, clickedOn }: Props) {
-	return (
-		<button
-			className={`w-full py-1 rounded text-center border ${
-				isSelected
-					? "text-white bg-white-dimmed border-white"
-					: " text-white-dimmed bg-dark-light border-dark-light"
-			}`}
-			onClick={() => clickedOn()}
-		>
-			{label}
-		</button>
-	);
+export function TabButton({ label, selected, onSelect }: Props) {
+  return (
+    <button
+      className={`w-full py-1 rounded text-center border ${
+        selected
+          ? "text-white bg-white-dimmed border-white"
+          : " text-white-dimmed bg-dark-light border-dark-light"
+      }`}
+      onClick={onSelect}
+    >
+      {label}
+    </button>
+  );
 }
