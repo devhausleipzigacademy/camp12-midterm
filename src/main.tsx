5e83a525ef19cfe793b64e017ada6a8e261662a2
Dan McAtee <danielmcatee@me.com>
2024-07-02 11:00:25 +0200

    change structure seat-price

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import SeatPrice from "./components/SeatPrice";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div className="bg-dark p-20 h-screen grid grid-cols-4 gap-20">
-      <SeatPrice number={1} category={{ title: " Front", price: 12.95 }} />
       <div className="bg-dark-light"></div>
       <div className="bg-white"></div>
       <div className="bg-white-dimmed"></div>
