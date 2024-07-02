fad9d36e7bd2200c2eea6701977aa1223cdfb166
ShaQay3Q <shaghay3gh@gmail.com>
2024-07-02 16:49:39 +0200

    movie-poster-page / buttons

--- src/components/PageButton.tsx
+++ src/components/PageButton.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import { cn } from "../utils/styling";
 
 type Props = {
-  active?: boolean;
-  page: number;
+	active?: boolean;
+	page: number;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const PageButton = ({ active = false, page, ...props }: Props) => {
-  return (
-    <button
-      className={cn(
-        "text-dark-light size-8 rounded-sm text-xs",
-        active ? "bg-yellow" : "bg-white-dimmed"
-      )}
-      {...props}
-    >
-      {page}
-    </button>
-  );
+	return (
+		<button
+			className={cn(
+				"text-dark-light size-8 rounded-sm text-xs ",
+				active ? "bg-yellow" : "bg-white-dimmed"
+			)}
+			{...props}
+		>
+			{page}
+		</button>
+	);
 };
