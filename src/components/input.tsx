3be2eccdf7d631271fd063c16d39ee70a71e216a
ShaQay3Q <shaghay3gh@gmail.com>
2024-07-03 16:26:53 +0200

    input.tsx updated

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,18 +1,37 @@
-import { IoIosSearch } from "react-icons/io";
+import React from "react";
 
-type Props = {} & React.DetailedHTMLProps<
-  React.InputHTMLAttributes<HTMLInputElement>,
-  HTMLInputElement
->;
+type Props = {
+  icon?: string;
+  placeholder: string;
+  inputType: "email" | "password" | "text";
+  pattern?: string;
+  minLength?: number;
+};
 
-export const Input = ({ ...props }: Props) => {
+export const Input = ({
+  icon,
+  placeholder,
+  inputType,
+  pattern,
+  minLength,
+}: Props) => {
   return (
     <label className="flex gap-5 items-center w-full py-3 px-5 rounded-full bg-dark-light border-2 border-dark-light focus-within:border-white-dimmed-heavy transition">
-      <IoIosSearch className="text-white-dimmed size-5" />
+      {/* <IoIosSearch className='text-white-dimmed size-5' /> */}
+      {icon && (
+        <img
+          src={icon}
+          alt={`${inputType} icon`}
+          className="text-white-dimmed w-5 h-5"
+        />
+      )}
       <input
-        type="text"
-        {...props}
-        className="bg-transparent font-medium text-white text-sm placeholder:leading-4 outline-none placeholder:text-white-dimmed"
+        type={inputType}
+        placeholder={placeholder}
+        pattern={pattern}
+        minLength={minLength}
+        aria-label={placeholder}
+        className="bg-transparent font-medium text-white text-sm placeholder:leading-4 outline-none text-white-dimmed"
       />
     </label>
   );
