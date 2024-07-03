90db219ad894b03d4e419e06782899d5efcf01da
focusedXmind <e.oettel@web.de>
2024-07-03 09:50:17 +0200

    changed name of getNowPlayingMovies Function and removed nowplayingmovies.tsx and genre.tsx from commit

--- src/services/tmdb.ts
+++ src/services/tmdb.ts
@@ -4,7 +4,7 @@ const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 console.log("API Key:", apiKey); // Add this line to check if the API key is correctly loaded
 const baseUrl = "https://api.themoviedb.org/3";
 
-export const fetchNowPlayingMovies = async () => {
+export const getNowPlayingMovies = async () => {
   try {
     const response = await axios.get(`${baseUrl}/movie/now_playing`, {
       params: {
