9af57bf321590a0c8c0594c1ca7cc8bb07d7a41b
Luzie Ahrens <luzie.ahrens@gmail.com>
2024-07-03 12:20:19 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import "./index.css";
 import { NavBar } from "./components/nav-bar";
-import GenreOverviewPage from "./routes/genreoverviewpage";
 
 const router = createBrowserRouter([
   {
@@ -77,8 +76,5 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    <div>
-      <GenreOverviewPage />
-    </div>
   </React.StrictMode>
 );
