8a4203996429156d9e7af4dcc2d58de2e18125d6
HaillSeitan <150841231+HaillSeitan@users.noreply.github.com>
2024-07-03 15:11:29 +0200

    renamed and cleaned up

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-
 import "./index.css";
-<<<<<<< HEAD
-import { HomepageHeader } from "./components/HomepageHeader";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <HomepageHeader userName={"Heinz-Werner"} userImage={""} />
-    <div className="bg-dark p-20 h-screen grid grid-cols-4 gap-20">
-      <div className="bg-dark-light"></div>
-      <div className="bg-white"></div>
-      <div className="bg-white-dimmed"></div>
-      <div className="bg-white-dimmed-heavy"></div>
-      <div className="bg-yellow"></div>
-      <div className="bg-red"></div>
-      <div className="bg-green"></div>
-    </div>
-=======
-import { NavBar } from "./components/nav-bar";
 
 const router = createBrowserRouter([
   {
@@ -93,6 +75,5 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
->>>>>>> main
   </React.StrictMode>
 );
