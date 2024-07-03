72cf16f996f29d9d2fcbb4734de1f74346a2a92c
Fred <frederik.koenig@posteo.de>
2024-07-03 12:19:29 +0200

    adjusted Font-size, font-weight, heroicon, items-center

--- src/components/section-title.tsx
+++ src/components/section-title.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { ChevronRightIcon } from "@heroicons/react/16/solid";
 
 type Props = {
   text: string;
@@ -7,26 +8,13 @@ type Props = {
 
 export function SectionTitle({ text, ShowSeeAll }: Props) {
   return (
-    <div className="flex justify-between">
-      <span className="text-white">{text}</span>
+    <div className="flex justify-between p-4 items-center">
+      <span className="text-white font-medium">{text}</span>
       {ShowSeeAll && (
-        <Link to="/Placeholder" className="text-yellow">
-          <div className="flex">
-            <span>See All</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5" // Use strokeWidth instead of stroke-width
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round" // Use strokeLinecap instead of stroke-linecap
-                strokeLinejoin="round" // Use strokeLinejoin instead of stroke-linejoin
-                d="m8.25 4.5 7.5 7.5-7.5 7.5"
-              />
-            </svg>
+        <Link to="/Placeholder" className="text-yellow text-sm">
+          <div className="flex flex-row items-center">
+            <span className="whitespace-nowrap">See All</span>
+            <ChevronRightIcon className="ml-2 w-5 h-5" />
           </div>
         </Link>
       )}
