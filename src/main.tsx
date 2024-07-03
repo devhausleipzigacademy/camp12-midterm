5437b705d80b41c57f8e6c8631b09e01980e9f61
HaillSeitan <150841231+HaillSeitan@users.noreply.github.com>
2024-07-03 09:27:39 +0200

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { NavBar } from "./components/nav-bar";
-import { Homepage } from "./routes/Homepage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
