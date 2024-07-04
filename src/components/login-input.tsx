d52cf68c86bd76a2e68e6f8dd44ff14dd4680c1a
Dan McAtee <danielmcatee@me.com>
2024-07-04 15:09:50 +0200

    fix login-input icon

--- src/components/login-input.tsx
+++ src/components/login-input.tsx
@@ -1,18 +1,24 @@
+import React from "react";
+
 type Props = {
-	icon?: JSX.Element;
+  icon?: JSX.Element;
 } & React.DetailedHTMLProps<
-	React.InputHTMLAttributes<HTMLInputElement>,
-	HTMLInputElement
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
 >;
 
-export function LoginInput({ icon: Icon, ...props }: Props) {
-	return (
-		<label className='px-5 py-3 bg-dark-light border-2 border-dark-light rounded-lg flex items-center gap-5 overflow-hidden focus-within:border-white-dimmed-heavy'>
-			<Icon className='h-6 w-auto text-white-dimmed' />
-			<input
-				className='bg-transparent text-white flex-grow outline-none placeholder:text-white-dimmed'
-				{...props}
-			/>
-		</label>
-	);
+export function LoginInput({ icon, ...props }: Props) {
+  return (
+    <label className="px-5 py-3 bg-dark-light border-2 border-dark-light rounded-lg flex items-center gap-5 overflow-hidden focus-within:border-white-dimmed-heavy">
+      {icon
+        ? React.cloneElement(icon, {
+            className: "h-6 w-auto text-white-dimmed",
+          })
+        : null}
+      <input
+        className="bg-transparent text-white flex-grow outline-none placeholder:text-white-dimmed"
+        {...props}
+      />
+    </label>
+  );
 }
