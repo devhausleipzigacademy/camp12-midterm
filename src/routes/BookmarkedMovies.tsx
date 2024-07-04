cc881a49e750a4477d9c0973165e1485c3810773
jsf@posteo.de <38862519+jsf030@users.noreply.github.com>
2024-07-04 14:38:08 +0200

    padding and bg-color

--- src/routes/BookmarkedMovies.tsx
+++ src/routes/BookmarkedMovies.tsx
@@ -11,7 +11,7 @@ import { PageButton } from "../components/page-button";
 const BookmarkedMovies: React.FC = () => {
   const [activePage, setActivePage] = useState<number>(1);
   return (
-    <div>
+    <div className="bg-dark px-5 py-8">
       <div className="grid grid-cols-2 gap-5">
         <div>
           <img
@@ -29,14 +29,13 @@ const BookmarkedMovies: React.FC = () => {
           <img src="/public/img/zodiac.jpg" alt="Zodiac" />
         </div>
       </div>
-      <div className="grid grid-cols-5 gap-2 p-4">
+      <div className="flex justify-between mt-5">
         {[1, 2, 3, 4, 5].map((page) => (
           <PageButton
             key={page}
             page={page}
             active={page === activePage}
             onClick={() => setActivePage(page)}
-            className="p-2"
           />
         ))}
       </div>
