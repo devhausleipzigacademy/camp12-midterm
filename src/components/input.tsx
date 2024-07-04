6375e97a8c74ef6f3fbf83ca993c66538e1d8e3c
Dan McAtee <danielmcatee@me.com>
2024-07-04 15:48:00 +0200

    add snap scrolling

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,19 +1,18 @@
 import { IoIosSearch } from "react-icons/io";
 
-type Props = {
-  icon: JSX.Element;
-} & React.DetailedHTMLProps<
+type Props = {} & React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
 >;
 
-export const Input = ({ icon, ...props }: Props) => {
+export const Input = ({ ...props }: Props) => {
   return (
     <label className="flex gap-5 items-center w-full py-3 px-5 rounded-full bg-dark-light border-2 border-dark-light focus-within:border-white-dimmed-heavy transition">
       <IoIosSearch className="text-white-dimmed size-5" />
       <input
-        className="bg-transparent font-medium text-white text-sm placeholder:leading-4 outline-none text-white-dimmed"
+        type="text"
         {...props}
+        className="bg-transparent font-medium text-white text-sm placeholder:leading-4 outline-none placeholder:text-white-dimmed"
       />
     </label>
   );
