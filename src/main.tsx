dfda4cd21e21e5c2c2c745dd35e068cedf0e2ac9
Nikita Nakropin <nikita@nakropin.com>
2024-07-04 13:55:42 +0200

    added dependencies for the barcode component to run

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Member from "./components/Member";
-import { NavBar } from "./components/NavBar";
+
+import TicketPage from "./layouts/TicketPage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NavBar></NavBar>
-    <div className="bg-dark p-20 h-screen grid grid-cols-4 gap-20">
-      <Member
-        name={"Jake Jakeson"}
-        role={"Foo the Fork"}
-        image={"https://i.imgflip.com/qixjk.jpg"}
-      />
-      <div className="bg-dark-light"></div>
-      <div className="bg-white"></div>
-      <div className="bg-white-dimmed"></div>
-      <div className="bg-white-dimmed-heavy"></div>
-      <div className="bg-yellow"></div>
-      <div className="bg-red"></div>
-      <div className="bg-green"></div>
-    </div>
+    <TicketPage
+      img="no image"
+      title="movie"
+      date="13 March"
+      time="14:00"
+      price={44.67}
+      seats={["C-1", "C-2", "C-3"]}
+      barcode="1231231"
+    />
   </React.StrictMode>
 );
