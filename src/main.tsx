afef9b84cde5017030cf70f32b4d0a6237e31f79
focusedXmind <e.oettel@web.de>
2024-07-08 13:20:50 +0200

    moviepage inkl api

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,9 @@ import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
 import BookmarkedMovies from "./routes/bookmarks";
 import { CastPage } from "./routes/cast-page";
 import { MoviePage } from "./components/movie-page";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -76,6 +79,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
