46396d02888e0a40f5cb2fdaf48e8827a66e209b
Hydrateurself <bensn@posteo.de>
2024-07-08 15:25:08 +0200

    Implemented whole seat Page Including functionality with seat selection and prize calc

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Member from "./components/Member";
-import { NavBar } from "./components/NavBar";
+import { ReservationPage } from "./layouts/reservation-page";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NavBar></NavBar>
-    <div className="bg-dark p-20 h-screen grid grid-cols-4 gap-20">
-      <Member
-        name={"Jake Jakeson"}
-        role={"Foo the Fork"}
-        image={"https://i.imgflip.com/qixjk.jpg"}
-      />
-      <div className="bg-dark-light"></div>
-      <div className="bg-white"></div>
-      <div className="bg-white-dimmed"></div>
-      <div className="bg-white-dimmed-heavy"></div>
-      <div className="bg-yellow"></div>
-      <div className="bg-red"></div>
-      <div className="bg-green"></div>
-    </div>
+    <ReservationPage />
   </React.StrictMode>
 );
