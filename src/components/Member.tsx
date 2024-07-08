52def9aa95b23d6c84911b009e25af162be62f17
Malibu <joellement@gmail.com>
2024-07-08 13:40:17 +0200

    new castpage branch

--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -1,21 +1,26 @@
-type Props = {
+export type Props = {
   image: string;
+
   name: string;
+
   role: string;
 };
 
-const Member = ({ image, name, role }: Props) => {
+export const Member = ({ image, name, role }: Props) => {
   return (
     <>
-      <div className="flex gap-5 justify-center items-center">
-        <img src={image} alt={name} className="size-16 object-cover" />
-        <div>
-          <h2 className="text-white font-bold text-sm mb-1">{name}</h2>
-          <p className="text-white-dimmed text-xs">{role}</p>
+      <div className="col-span-full row-span-1">
+        {" "}
+        <div className="flex gap-5 justify-start items-center">
+          <img src={image} alt={name} className="size-16 object-cover" />
+
+          <div>
+            <h2 className="text-white font-bold text-sm mb-1">{name}</h2>
+
+            <p className="text-white-dimmed text-xs">{role}</p>
+          </div>
         </div>
       </div>
     </>
   );
 };
-
-export default Member;
