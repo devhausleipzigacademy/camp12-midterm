6eb419699bc0988cc2bca3e13dfce4f772a43110
Hydrateurself <bensn@posteo.de>
2024-07-10 14:05:21 +0200

    created custom Hook for fetching Ids. Fetched ids from local storage and mapped to grid. Fixed issue with pages stuck to grid when only one movie is in grid

---
+++ src/hooks/useGetMoviesById.tsx
@@ -0,0 +1,39 @@
+import { useQueries } from "@tanstack/react-query";
+import axios from "axios";
+import { Movie } from "../types/movie";
+
+export const useGetMoviesById = () => {
+  const ids = [748783, 46195, 280180, 519182, 1008409];
+  // change ids with storedIds for getting actual movies from local storage
+
+  // const storedIds = JSON.parse(localStorage.Movies);
+
+  const results = useQueries({
+    queries: ids.map((id) => ({
+      queryKey: ["movie", id],
+      queryFn: async () => {
+        const { data } = await axios.get<Movie>(
+          `https://api.themoviedb.org/3/movie/${id}`,
+          {
+            params: {
+              api_key: import.meta.env.VITE_TMDB_API_KEY,
+            },
+          }
+        );
+        return data;
+      },
+    })),
+  });
+
+  const isLoading = results.some((result) => result.isLoading);
+  const isError = results.some((result) => result.isError);
+  const movies = results
+    .map((result) => result.data)
+    .filter((movie) => movie !== undefined) as Movie[];
+
+  return {
+    isLoading,
+    isError,
+    movies,
+  };
+};
