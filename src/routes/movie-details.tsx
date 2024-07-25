9e307f30733238af91e92bb473730662fc9195c6
Fred <frederik.koenig@posteo.de>
2024-07-25 11:22:52 +0200

    fixed navigation from movie details to go back to the previous page, not only movies

--- src/routes/movie-details.tsx
+++ src/routes/movie-details.tsx
@@ -75,6 +75,10 @@ export function MovieDetails() {
 
   // see the actual data entries from tmdb
 
+  function handleGoBack() {
+    navigate(-1);
+  }
+
   return (
     // background
     <section className="flex flex-col h-full min-h-screen bg-dark px-4 overflow-x-hidden">
@@ -83,7 +87,7 @@ export function MovieDetails() {
         {/* Header */}
         <div className="flex flex-row justify-between my-2 h-16 items-center">
           {/* link back to movies */}
-          <NavLink to="/movies">
+          <button onClick={handleGoBack}>
             <svg
               className="h-4 aspect-square"
               viewBox="0 0 16 16"
@@ -92,7 +96,7 @@ export function MovieDetails() {
             >
               <path d="M5.14671 8.35335C5.05308 8.2596 5.00049 8.13252 5.00049 8.00002C5.00049 7.86752 5.05308 7.74044 5.14671 7.64669L10.1467 2.64669C10.1925 2.59756 10.2477 2.55816 10.309 2.53083C10.3704 2.50351 10.4366 2.48881 10.5037 2.48763C10.5708 2.48644 10.6375 2.49879 10.6998 2.52394C10.762 2.54909 10.8186 2.58652 10.8661 2.634C10.9136 2.68147 10.951 2.73803 10.9761 2.80029C11.0013 2.86255 11.0136 2.92923 11.0124 2.99637C11.0113 3.0635 10.9966 3.12971 10.9692 3.19105C10.9419 3.25238 10.9025 3.30758 10.8534 3.35335L6.20672 8.00002L10.8534 12.6467C10.9025 12.6925 10.9419 12.7477 10.9692 12.809C10.9966 12.8703 11.0113 12.9365 11.0124 13.0037C11.0136 13.0708 11.0013 13.1375 10.9761 13.1998C10.951 13.262 10.9136 13.3186 10.8661 13.366C10.8186 13.4135 10.762 13.451 10.6998 13.4761C10.6375 13.5013 10.5708 13.5136 10.5037 13.5124C10.4366 13.5112 10.3704 13.4965 10.309 13.4692C10.2477 13.4419 10.1925 13.4025 10.1467 13.3534L5.14671 8.35335Z" />
             </svg>
-          </NavLink>
+          </button>
 
           <h1 className="text-white text-base font-bold">Movie Detail</h1>
           <HeartIcon
